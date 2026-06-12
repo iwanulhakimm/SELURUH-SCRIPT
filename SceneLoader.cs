d4074f6ee003ad7e8e277e1e3a289863cde38d59
iwanulhakimm <iwanulhakim1@gmail.com>
2026-06-13 00:06:53 +0700

    Remove XML comments from SceneLoader class

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -3,42 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    /// <summary>
-    /// Pindah ke scene berdasarkan nama
-    /// </summary>
     public void LoadSceneByName(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    /// <summary>
-    /// Pindah ke scene berdasarkan index/nomor
-    /// </summary>
     public void LoadSceneByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
 
-    /// <summary>
-    /// Pindah ke scene berikutnya
-    /// </summary>
+
     public void LoadNextScene()
     {
         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
         SceneManager.LoadScene(nextSceneIndex);
     }
 
-    /// <summary>
-    /// Reload scene yang sedang aktif
-    /// </summary>
     public void ReloadCurrentScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    /// <summary>
-    /// Pindah scene dengan loading asynchronous (untuk scene besar)
-    /// </summary>
     public void LoadSceneAsync(string sceneName)
     {
         StartCoroutine(LoadSceneAsyncCoroutine(sceneName));
@@ -50,7 +36,6 @@ public class SceneLoader : MonoBehaviour
         
         while (!asyncLoad.isDone)
         {
-            // Progress: asyncLoad.progress (0 - 0.9)
             Debug.Log("Loading: " + (asyncLoad.progress * 100) + "%");
             yield return null;
         }
